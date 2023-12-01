71a7aab61537ce73c8945bea1c6358a7283ef699
Varth Dader <varthdader@users.noreply.github.com>
2023-12-01 11:54:15 -0700

    Update dcsync_monitoring.ps1
    Update Again

--- dcsync_monitoring.ps1
+++ dcsync_monitoring.ps1
@@ -1,6 +1,11 @@
 Import-Module ActiveDirectory
 
-cd 'AD:DC=littletongov,DC=org'
+cd 'AD:DC=CLIENT-DOMAIN,DC=LOCAL
+
+# DS Events to Monitor For
+# DS-Replication-Get-Changes-All = '1131f6ad-9c07-11d1-f79f-00c04fc2dcd2'
+# DS-Replication-Get-Changes-In-Filtered-Set = '89e95b76-444d-4c62-991a-0facbeda640c'
+# DS-Replication-Get-Changes = '1131f6aa-9c07-11d1-f79f-00c04fc2dcd2'
 
 $AllReplACLs = (Get-AcL).Access | Where-Object `
 {$_.ObjectType -eq '1131f6ad-9c07-11d1-f79f-00c04fc2dcd2' -or
